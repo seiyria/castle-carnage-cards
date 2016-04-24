1f08c7367b21f39eec4bb7af1185cd21c395d500
Kyle J. Kemp <kyle@seiyria.com>
2016-04-24 07:55:30 -0500

    the actual common file

--- common.js
+++ common.js
@@ -1,88 +1,182 @@
 
 var _ = require('lodash');
-var fs = require('fs');
 
-var GoogleSpreadsheet = require('google-spreadsheet');
-var doc = new GoogleSpreadsheet('1JpObTzg2uAwfpbs-UOZ5rehuwX02VoBHLEa6v56ZCAA');
+var COLORS = {
+    DM:         '#000000',
+    Cleric:     '#008080',
+    Mage:       '#000099',
+    Ranger:     '#AA7243',
+    Thief:      '#003C00',
+    Warrior:    '#990000',
+    Merchant:   '#000000', //TODO
+    Alchemist:  '#000000', //TODO
+    Monk:       '#000000', //TODO
+    Item:       '#CC8400',
+    Relic:      '#49311C',
+    Trap:       '#49311C'
+};
 
-var creds = require('./CastleCarnageService.json');
+/* // used for sending to TheGameCrafter
+var SIZES = {
+    WIDTH: 6.35,
+    HEIGHT: 8.89
+};
+*/
 
-var common = require('./common');
-var PROCESS_SHEETS = common.PROCESS_SHEETS;
-var backFromCard = common.backFromCard;
-var tplFromCard = common.tplFromCard;
-var processData = common.processData;
-var getMetadata = common.getMetadata;
-var addLine = common.addLine;
-var outputtedCode = common.base();
+var SIZES = {
+    WIDTH: 6.4,
+    HEIGHT: 9.0 //should be 8.7, but eh - 9.0 fits better in sleeves (and doesn't cut off)
+};
 
-var writeCards = (cards) => {
-    for(var i = 0; i < cards.length; i++) {
-        outputtedCode = addLine(outputtedCode, (cards[i].isBack ? backFromCard : tplFromCard)(cards[i], i+1));
+var PROCESS_SHEETS = ['DM', 'Cleric', 'Mage', 'Thief', 'Warrior',/*'Ranger', 'Merchant', 'Monk', 'Alchemist', */ 'Item', 'Relic', 'Trap'];
+
+var outputtedCode = `
+PAGE=21, 29.7, "PORTRAIT", HV
+DPI=300
+CARDSIZE=${SIZES.WIDTH}, ${SIZES.HEIGHT}
+MARGINS=0.45, 0.45, 0.45, 0.45
+GAP=0, 0
+BORDER=rectangle,#000000,0.1,MARK
+
+[CardFont] = "Arial"
+[CardFlags] = TNF
+
+[TitleFont] = [CardFont], 18, [CardFlags], #000000
+[TextFont] = [CardFont], 10, [CardFlags], #000000
+[SubtitleFont] = [CardFont], 12, [CardFlags], #000000
+[SubtextFont] = [CardFont], 10, [CardFlags], #000000
+[SubsubtextFont] = [CardFont], 10, [CardFlags], #000000
+[ValueFont] = [CardFont], 10, [CardFlags], #ffffff
+`;
+
+var addLine = (outputtedCode, line) => {
+    return `${outputtedCode}${line}\n`;
+};
+
+var processRow = (title, rowData, metadata) => {
+    var cards = [];
+    for(var i = 0; i < (rowData.quantity || 0); i++) {
+        cards.push({
+            cardClass: title,
+            name: rowData.name,
+            subtitle: rowData.subtitle,
+            value: rowData.value,
+            trigger: rowData.trigger,
+            target: rowData.target,
+            text: rowData.text,
+            subtext: rowData.subtext,
+            revision: metadata.revision
+        });
     }
-    fs.writeFileSync('cc-gen.txt', outputtedCode);
+    return cards;
 };
 
-var splitCardsAndGetBacks = (cards) => {
-    return _(cards)
-        .chunk(9)
-        .map(chunk => {
-            const miniChunks = _.chunk(chunk, 3);
-            const emptyCard = { name: '', subtitle: '', trigger: '', target: '', text: '', subtext: '',
-            value: '', cardClass: '' };
-
-            while(miniChunks.length < 3) miniChunks.push([emptyCard, emptyCard, emptyCard]);
-
-            _.each(miniChunks, miniChunk => {
-                while(miniChunk.length < 3) miniChunk.push(emptyCard);
-            });
-
-            const miniChunkBacks = _.map(miniChunks, (miniChunk, miniIdx) => {
-                return _(miniChunk)
-                    .map((tinyChunk, tinyIdx) => {
-                        const refChunk = chunk[(miniIdx*3)+tinyIdx];
-                        return {
-                            isBack: true,
-                            showClassSubtitle: refChunk && refChunk.subtitle === 'Class',
-                            text: refChunk ? refChunk.cardClass : ''
-                        };
-                    })
-                    .reverse()
-                    .value();
-            });
-
-            return [miniChunks, miniChunkBacks];
-        })
-        .flattenDeep()
-        .value();
-};
-
-doc.useServiceAccountAuth(creds, () => {
-    doc.getInfo((err, info) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-
-        getMetadata(info).then(metadata => {
-            const revision = metadata.revision;
-            console.info(`Working with revision ${revision}.`);
-
-            Promise.all(_.map(info.worksheets, (sheet) => {
-                if(!_.includes(PROCESS_SHEETS, sheet.title)) return;
-                return processData(sheet, metadata);
-            })).then(data => {
-                const cards = _(data).compact().flattenDeep().value();
-                
-                console.info(`Total cards: ${cards.length}.`);
-                if(cards.length % 9 !== 0) {
-                    console.warn(`Warning: card count not divisible by 9.`);
-                }
-
-                writeCards(splitCardsAndGetBacks(cards));
-                console.info('Done.');
-
-            }).catch(console.error);
+var processData = (sheet, metadata) => {
+    return new Promise((resolve, reject) => {
+        sheet.getRows({
+            offset: 1
+        }, (e, rows) => {
+            if(e) reject(e);
+            const deckCards = _.flattenDeep(_.map(rows, row => processRow(sheet.title, row, metadata)));
+            console.info(`Built ${sheet.title} deck (${deckCards.length} cards)`);
+            resolve(deckCards);
         });
     });
-});
+};
+
+var tplFromCard = (card, index) => {
+
+    const cardSubtitle = (subtitle) => {
+        if(subtitle === 'Class Skill') return `${card.cardClass} Skill`;
+        return subtitle;
+    };
+
+    const textHeight = card.subtext ? 3 : 6;
+    const cardTextStart = card.trigger || card.target ? 3 : 2;
+
+    var base = `
+font=[TitleFont]
+text=${index}, "${card.name}", 0, 0, 6.4, 1, "center", "center"
+
+font=[SubtitleFont]
+text=${index}, "${cardSubtitle(card.subtitle)}", 0, 1, 6.4, 1, "center", "top"
+
+font=[SubsubtextFont]
+text=${index}, "${card.trigger}", 0, 2, 3.2, 1, "center", "top"
+
+font=[SubsubtextFont]
+text=${index}, "${card.target}", 3.2, 2, 3.2, 1, "center", "top"
+
+font=[TextFont]
+htmltext=${index}, "${card.text.split('\n').join('<br>')}", 0.8, ${cardTextStart}, 4.8, ${textHeight}
+
+font=[SubtextFont]
+text=${index}, "${card.subtext}", 0.8, 6, 4.8, 3, "left", "wordwrap"
+`;
+
+    const revision = `
+font=[SubtextFont]
+text=${index}, "Revision ${card.revision}", 0.5, 8.5, 1, 0.5, "center", "center"
+`
+
+    const value = `
+font=[ValueFont]
+ELLIPSE=${index}, 5, 8.5, 1, 0.5, #000000
+text=${index}, "${card.value}", 5, 8.5, 1, 0.5, "center", "center"
+`;
+
+    if(card.revision) {
+        base = base + revision;
+    }
+
+    if(card.value) {
+        base = base + value;
+    }
+
+    return base;
+};
+
+var cardBackingDisplay = (text) => {
+    switch(text) {
+        case 'Trap':
+        case 'Relic': return 'Dungeon';
+        default: return text;
+    }
+}
+
+var backFromCard = (card, index) => {
+    var base = `
+font=[TitleFont]
+text=${index}, "${cardBackingDisplay(card.text)}", 0, 0, 6.4, 6.4, "center", "center"
+    `;
+
+    const subClassText = `
+font=[TitleFont]
+text=${index}, "Class Card", 0, 6.4, 6.4, 2.8, "center", "center"
+    `;
+
+    if(card.showClassSubtitle) {
+        base = base + subClassText;
+    }
+
+    return base;
+};
+
+var getMetadata = (spreadsheet) => {
+    return new Promise((resolve, reject) => {
+        const sheet = _.find(spreadsheet.worksheets, { title: 'Metadata' });
+        sheet.getRows({ offset: 1 }, (e, rows) => {
+            if(e) return reject(e);
+            resolve(rows[0]);
+        });
+    });
+}
+
+module.exports.COLORS = COLORS;
+module.exports.PROCESS_SHEETS = PROCESS_SHEETS;
+module.exports.backFromCard = backFromCard;
+module.exports.tplFromCard = tplFromCard;
+module.exports.processData = processData;
+module.exports.getMetadata = getMetadata;
+module.exports.addLine = addLine;
+module.exports.base = () => outputtedCode;
